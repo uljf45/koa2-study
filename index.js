3153c24655ec4cb736fd3f79574d613df6a5e220
chenzemin <chen.zemin@bw.com>
2018-05-17 16:08:58 +0800

    koa2使用session中间件

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const Koa = require('koa') // koa v2
 const path = require('path')
 const static = require('koa-static')
+const session = require('koa-session-minimal')
+const MysqlSession = require('koa-mysql-session')
 
 const app = new Koa()
+
+//配置存储session信息的mysql
+let store = new MysqlSession({
+  user: 'root',
+  password: 'abc1234!',
+  database: 'koa_demo',
+  host: 'localhost'
+})
+
+//存放sessionId的cookie配置
+let cookie = {
+  maxAge: '',
+  expires: '',
+  path: '',
+  domain: '',
+  httpOnly: '',
+  overwrite: '',//是否允许重写
+  secure: '',
+  sameSite: '',
+  signed: ''
+}
+
+app.use(session({
+  key: 'SESSION_ID',
+  store: store,
+  cookie: cookie
+}))
 
 //静态资源目录
 const staticPath = './static'
@@ -12,22 +41,15 @@ app.use(static(
 ))
 
 app.use( async ( ctx ) => {
-  if ( ctx.url === '/index' ) {
-    ctx.cookies.set(
-      'cid',
-      'hello world',
-      {
-        domain: 'localhost',
-        path: '/',
-        maxAge: 10 * 60 * 1000, //cookie有效时长
-        expires: new Date('2018-09-09'), //cookies失效时间
-        httpOnly: false, //是否只用于http请求中获取
-        overwrite: false //是否允许重写
-      }
-    )
-    ctx.body = 'cookie is ok'
-  } else {
-    ctx.body= 'hello world'
+  if ( ctx.url === '/set' ) {
+    ctx.session = {
+      user_id: Math.random().toString(36).substr(2),
+      count: 0
+    }
+    ctx.body = ctx.session
+  } else if ( ctx.url === '/get') {
+    ctx.session.count = ctx.session.count + 1
+    ctx.body = ctx.session
   }
 })
 
