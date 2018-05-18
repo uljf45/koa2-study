74d184c853b44d2acd1baf3a0e94a1be693edf48
chenzemin <chen.zemin@bw.com>
2018-05-18 10:29:42 +0800

    原生koa2实现jsonp

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-const Koa = require('koa') // koa v2
-const views = require('koa-views')
-const path = require('path')
-const static = require('koa-static')
+const Koa = require('koa')
 const app = new Koa()
 
-const { uploadFile } = require('./util/upload')
+app.use(async (ctx) => {
+  if (ctx.method === 'GET' && ctx.url.split('?')[0] === '/getData.jsonp') {
+    let callbackName = ctx.query.callback || 'callback'
+    let returnData = {
+      success: true,
+      data: {
+        text: 'this is a jsonp api',
+        time: new Date().getTime()
+      }
+    }
 
-app.use(views(path.join(__dirname, './view'), {
-  extension: 'ejs'
-}))
-
-const staticPath = './static'
-
-app.use(static(
-  path.join( __dirname, staticPath)
-))
-
-app.use( async ( ctx ) => {
-  if ( ctx.method === 'GET' ) {
-    let title = 'upload pic async'
-    await ctx.render('index', {
-      title
-    })
-  } else if ( ctx.url === '/api/picture/upload.json' && ctx.method === 'POST' ) {
-    //上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join(__dirname, 'static/image' )
-
-    //上传文件事件
-    result = await uploadFile ( ctx, {
-      fileType: 'album', // common or album
-      path: serverFilePath
-    })
-    ctx.body = result
+    let jsonpStr = `;${callbackName}(${JSON.stringify(returnData)})`
+  
+    ctx.type = 'text/javascript'
+    ctx.body = jsonpStr
   } else {
-    ctx.body = '<h1>404!!!</h1>'
+    ctx.body = 'hello jsonp'
   }
 })
 
-app.listen(3000)
-console.log('the server is starting at port 3000')
+app.listen(3000, () => {
+  console.log('[demo] is starting at port 3000')
+})
